d60a36784a031cfc8bf039243ffe5e57b3c0da2e
Zenrac <zenrac@outlook.fr>
2018-12-05 01:15:40 +0100

    Update __init__.py

--- lavalink/__init__.py
+++ lavalink/__init__.py
@@ -6,6 +6,7 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Devoxin'
 __version__ = '3.0.0'
 
+
 from .client import Client
 from .events import TrackStartEvent, TrackStuckEvent, TrackExceptionEvent, TrackEndEvent, QueueEndEvent
 from .models import BasePlayer, DefaultPlayer, AudioTrack, NoPreviousTrack, InvalidTrack
